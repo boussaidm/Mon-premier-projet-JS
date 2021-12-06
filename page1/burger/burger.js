c793913af8489b29ac5c1ecd9939309e32e24d20
13010-55-06 <13010-55-06@exchange.ad.afpanet>
2021-12-06 09:06:54 +0100

    SNK terminé

--- page1/burger/burger.js
+++ page1/burger/burger.js
@@ -7,3 +7,11 @@ function menu() {
 }
 
 burger.addEventListener('click', menu);
+
+var li =  document.querySelectorAll('.navbar__link')
+
+for(var i = 0; i<li.length; i++) {
+    li[i].addEventListener('click', ()=> {
+        navbar.classList.toggle('showNav');
+    })
+}
